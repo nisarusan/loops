2084aec9dc038546eb7d21efa90eb9e105ef1cfa
Nisar <145307487+nisarusan@users.noreply.github.com>
2023-10-11 03:52:58 +0200

    files

--- loops.js
+++ loops.js
@@ -6,6 +6,7 @@
 // ***
 // ****
 // *****
+//
 
 // Tip: je kunt de .repeat() methode gebruiken om een karakter een n aantal keer te herhalen...
 // Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
